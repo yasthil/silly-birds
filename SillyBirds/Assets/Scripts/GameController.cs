ad42413f9dc32a3e5eff80730fc030ef9899adf4
Yasthil Bhagwandeen <deiago147@gmail.com>
2017-12-28 22:29:04 +0200

    Added GameController and ObjectPooler functionality. Birds are now spawning using object pooling

---
+++ SillyBirds/Assets/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Responsible for the general controlling of the game
+/// </summary>
+public class GameController : MonoBehaviour {
+
+    private bool _gameRunning;
+
+	// Use this for initialization
+	void Start () {
+
+        _gameRunning = true;
+        StartCoroutine(SpawnBirds());
+	}
+
+    /// <summary>
+    /// Spawns Birdies using the ObjectPooler
+    /// </summary>
+    private IEnumerator SpawnBirds()
+    {
+        GameObject sillyBird = null;
+
+        // game loop - this should be more performant than using Update() on every frame
+        while(_gameRunning)
+        {
+            sillyBird = ObjectPooler.Instance.GetPooledObject();
+            if (sillyBird != null)
+            {
+                // set random position for silly bird
+
+                // set it to active
+                sillyBird.SetActive(true);
+            }
+
+            // hard coding spawn for every 2 seconds
+            // TODO: Have a min, max spawn range and randomly choose from there
+            yield return new WaitForSeconds(2);
+        }
+    }
+	
+}
---
+++ SillyBirds/Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This Singleton is responsible for pooling objects. Using this we can avoid 
+/// constant spawning and destroying of objects in our scene.
+/// </summary>
+public class ObjectPooler : MonoBehaviour
+{
+    public int _amountToPool;
+    public static ObjectPooler Instance;
+    public List<GameObject> _objectsToPool;
+    private List<GameObject> _pooledObjects;
+    private List<GameObject> _inActiveObjects;
+
+    void Awake()
+    {
+        // Singleton implementation
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+            _inActiveObjects = new List<GameObject>();
+            return;
+        }
+        Destroy(this.gameObject);
+    }
+
+    void Start()
+    {
+        // Instantiate the objectsToPool
+        _pooledObjects = new List<GameObject>();
+        for (int i = 0; i < _amountToPool; i++)
+        {
+            // randomize their order
+            GameObject obj = (GameObject)Instantiate(_objectsToPool[Random.Range(0, _objectsToPool.Count)]);
+
+            // set all as inactive
+            obj.SetActive(false);
+            _pooledObjects.Add(obj);
+        }
+    }
+    
+    public void RemovePooledObjects()
+    {
+        for (int i = 0; i < _pooledObjects.Count; i++)
+        {
+            _pooledObjects[i].SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// Returns a random pooled GameObject that was inactive 
+    /// </summary>
+    /// <returns></returns>
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < _pooledObjects.Count; i++)
+        {
+            if (!_pooledObjects[i].activeInHierarchy)
+            {
+                // keep a bag of inactive objects so we can randomly choose from them
+                // this ensures that all items have a chance to be returned
+                _inActiveObjects.Add(_pooledObjects[i]);
+            }
+        }
+
+        GameObject randomGameOject = GetRandomInActiveObject();
+
+        // clear list
+        _inActiveObjects.Clear();
+        return randomGameOject;
+    }
+    /// <summary>
+    //  Select a random object from the inactive bag and return it
+    /// </summary>
+    /// <returns></returns>
+    private GameObject GetRandomInActiveObject()
+    {
+        int randomIndex = Random.Range(0, _inActiveObjects.Count);
+        return _inActiveObjects[randomIndex];
+    }
+}
