53645efa7fec09471402c1347e4b44c5e17d67be
Yasthil Bhagwandeen <deiago147@gmail.com>
2017-12-29 00:19:31 +0200

    Added basic, linear movement for birds

---
+++ SillyBirds/Assets/Scripts/BirdMover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Responsible for moving a single bird
+/// </summary>
+public class BirdMover : MonoBehaviour {
+
+    public float Speed = 0.5f;
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(transform.position.x - Time.deltaTime * Speed, transform.position.y, transform.position.z);
+    }
+}
