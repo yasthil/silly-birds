033942cf52a5abdc6250e6c8b4c336659a7ea032
Yasthil Bhagwandeen <deiago147@gmail.com>
2017-12-28 22:44:10 +0200

    Spawning the birds on screen and in random X,Y positions

--- SillyBirds/Assets/Scripts/GameController.cs
+++ SillyBirds/Assets/Scripts/GameController.cs
@@ -8,9 +8,18 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
 
     private bool _gameRunning;
+    private const float BIRD_SPAWN_MIN_Y_POS = -3.9f;
+    private const float BIRD_SPAWN_MAX_Y_POS = 3.7f;
+    private const float BIRD_SPAWN_MIN_X_POS = -14f;
+    private const float BIRD_SPAWN_MAX_X_POS = 14f;
 
-	// Use this for initialization
-	void Start () {
+    // some defaults spawn positions
+    private float _spawnPositionX = 0f;        
+    private float _spawnPositionY = 0f;        
+    private float _spawnPositionZ = -60f;       // set the z-order so we know exactly how far from the camera they should be
+
+    // Use this for initialization
+    void Start () {
 
         _gameRunning = true;
         StartCoroutine(SpawnBirds());
@@ -22,14 +31,21 @@ public class GameController : MonoBehaviour {
     private IEnumerator SpawnBirds()
     {
         GameObject sillyBird = null;
+        float randomBirdYPos;
+        float randomBirdXPos;
 
         // game loop - this should be more performant than using Update() on every frame
-        while(_gameRunning)
+        while (_gameRunning)
         {
             sillyBird = ObjectPooler.Instance.GetPooledObject();
             if (sillyBird != null)
             {
-                // set random position for silly bird
+                // get a random X and Y positions that's within some bounds
+                randomBirdYPos = UnityEngine.Random.Range(BIRD_SPAWN_MIN_Y_POS, BIRD_SPAWN_MAX_Y_POS);
+                randomBirdXPos = UnityEngine.Random.Range(BIRD_SPAWN_MIN_X_POS, BIRD_SPAWN_MAX_X_POS);
+
+                // set the bird's position
+                sillyBird.transform.position = new Vector3(randomBirdXPos, randomBirdYPos, _spawnPositionZ);
 
                 // set it to active
                 sillyBird.SetActive(true);
