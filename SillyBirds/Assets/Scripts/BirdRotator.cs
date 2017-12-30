af68638c3ab76a93c756f20f5f6411da99785e70
Yasthil Bhagwandeen <deiago147@gmail.com>
2017-12-31 00:27:09 +0200

    Added Main Menu, Scene loading and added TextMeshPro

---
+++ SillyBirds/Assets/Scripts/BirdRotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdRotator : MonoBehaviour
+{
+
+    public float Speed = 0.5f;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * (Speed * Time.deltaTime));
+    }
+}
---
+++ SillyBirds/Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Simple scene loader helper
+/// </summary>
+public class LoadSceneOnClick : MonoBehaviour
+{
+
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+	
+}
