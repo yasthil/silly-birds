563da40f7a5cc2918cda137d93e987de830dbe56
Yasthil Bhagwandeen <deiago147@gmail.com>
2017-12-31 01:45:23 +0200

    Added sound effects and background music

--- SillyBirds/Assets/Scripts/BirdController.cs
+++ SillyBirds/Assets/Scripts/BirdController.cs
@@ -6,6 +6,7 @@ public class BirdController : MonoBehaviour
 {
     public GameObject _explosion;
     public Color _particleColor;
+    public AudioClip _explodeSound;
 
     /// <summary>
     /// When a bird leaves the boundary, don't destroy it - set it to inactive
@@ -29,6 +30,9 @@ public class BirdController : MonoBehaviour
 
     public void Explode()
     {
+        // play explode sound
+        PlayExplodeSound();
+
         // play burst animation
         Vector3 pos = transform.position;
      
@@ -40,6 +44,13 @@ public class BirdController : MonoBehaviour
         explosion.GetComponent<ParticleSystemRenderer>().material.color = _particleColor;
     }
 
-
+    private void PlayExplodeSound()
+    {
+        // create an empty GameObject to attach the sound to
+        GameObject soundContainerGO = new GameObject("ObstacleExplodeSoundContainer");
+        AudioSource audioSource = soundContainerGO.AddComponent<AudioSource>();       
+        audioSource.clip = _explodeSound;
+        audioSource.Play();
+    }
 
 }
